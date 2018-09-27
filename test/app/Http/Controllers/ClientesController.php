5b7bd09bcddc4cbc55da3338795f585ada321021
EC2 Default User <ec2-user@ip-172-31-37-52.ec2.internal>
2018-09-27 17:35:25 +0000

    test cliente

---
+++ test/app/Http/Controllers/ClientesController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Cliente;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+class ClientesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $users = DB::table('users')->get();
+        // dd($users);
+        // return $users;
+        return view('welcome')->with(['clientes'=>$users]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        // dd($request);
+        $cliente = new Cliente;
+        $cliente->name = $request->name;
+        $cliente->last_name = $request->last_name;
+        $cliente->document = $request->document;
+        $cliente->email = $request->email;
+        $cliente->phone = $request->phone;
+        $cliente->address = $request->address;
+
+        $cliente->save();
+        
+        $users = DB::table('users')->get();
+        // dd($users);
+        return view('welcome')->with(['clientes'=>$users]);
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Cliente  $cliente
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cliente $cliente)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Cliente  $cliente
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Cliente $cliente)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Cliente  $cliente
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Cliente $cliente)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Cliente  $cliente
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cliente $cliente)
+    {
+        //
+    }
+}
---
+++ test/resources/views/welcome.blade.php
@@ -0,0 +1,119 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.12.1/bootstrap-table.min.css">
+        
+        <link rel="stylesheet" href="https://6e06cb2ff59a4c079f07907b449835c7.vfs.cloud9.us-east-1.amazonaws.com:8080/css/myStyle.css" type="text/css" />
+    </head>
+    <body>
+        <div class="container-fluid __home">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="jumbotron">
+                            <table data-toggle="table" id="table"
+                               data-pagination="true"
+                               data-side-pagination="server"
+                               data-page-list="[5, 10, 20, 50, 100, 200]"
+                               data-search="true"
+                            >
+                                <thead>
+                                <tr>
+                                    <th data-field="name">nombre</th>
+                                    <th data-field="last_name">apellido</th>
+                                    <th data-field="document">documento</th>
+                                    <th data-field="email">correo</th>
+                                    <th data-field="phone">telefono</th>
+                                    <th data-field="address">dirección</th>
+                                    
+                                </tr>
+                                </thead>
+                                    
+                                    
+                                <tbody>
+                                    <?php
+                                        foreach ($clientes as $data) {
+                                            echo '<tr>
+                                            <td>'.$data->name.'</td>
+                                            <td>'.$data->last_name.'</td>
+                                            <td>'.$data->document.'</td>
+                                            <td>'.$data->email.'</td>
+                                            <td>'.$data->phone.'</td>
+                                            <td>'.$data->address.'</td>
+                                            </tr>';
+                                        }
+                                    ?>
+                                </tbody>    
+                                    
+                            </table>
+                            
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="jumbotron">
+                        <div>
+                            <form method="POST" action="/cliente/create">
+                                @csrf
+                            <div class="form-group">
+                                <label for="nombre">Nombre</label>
+                                <input type="text" class="form-control" name="name" id="nombre" placeholder="Nombre">
+                            </div>
+                            <div class="form-group">
+                                <label for="apellido">Apellido</label>
+                                <input type="text" class="form-control" name="last_name" id="apellido" placeholder="Apellido">
+                            </div>
+                            <div class="form-group">
+                                <label for="documento">Documento</label>
+                                <input type="text" class="form-control" name="document" id="documento" placeholder="Documento">
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input type="email" class="form-control" name="email" id="email" placeholder="Email">
+                            </div>
+                            <div class="form-group">
+                                <label for="telefono">Telefono</label>
+                                <input type="text" class="form-control" name="phone" id="telefono" placeholder="Telefono">
+                            </div>
+                            <div class="form-group">
+                                <label for="direccion">dirección</label>
+                                <input type="text" class="form-control" name="address" id="direccion" placeholder="dirección">
+                            </div>
+                            
+                                
+                            <button type="submit" id="crear" class="btn btn-success">Crear</button>
+                        </div>
+                        </form>
+                    </div>
+                </div>    
+            </div>
+        </div>
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.12.1/bootstrap-table.min.js"></script>
+        
+        <!-- Latest compiled and minified Locales -->
+        <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.12.1/locale/bootstrap-table-es-AR.min.js"></script>    
+        <script>
+        $(document).ready(function() {
+            // console.log('ok');
+            var info={};
+            var url='https://6e06cb2ff59a4c079f07907b449835c7.vfs.cloud9.us-east-1.amazonaws.com:8080/';
+            
+            
+        });    
+        </script>
+    </body>
+</html>
